5fd8b9533cd03f0dc491c9f1e75b7e4f7ac02201
Cerveau++ <77627170+alban-okoby@users.noreply.github.com>
2023-02-18 20:10:51 +0000

    Add items to the list successfully �

--- todolist/src/app/app.component.ts
+++ todolist/src/app/app.component.ts
@@ -24,4 +24,10 @@ export class AppComponent {
     return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
   }
 
+  addItem(description: string) {
+    this.allItems.push({
+      description, done: false
+    });
+  }
+
 }
